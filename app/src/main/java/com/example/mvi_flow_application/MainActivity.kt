4ffd9fcb34e7c4348064bcc277d882acff0fc578
Kassym <kassym@kolesa.kz>
2022-09-16 18:10:51 +0600

    Добавил Макет экрана

---
+++ app/src/main/java/com/example/mvi_flow_application/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mvi_flow_application
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(p0: View?) {
+
+    }
+}
